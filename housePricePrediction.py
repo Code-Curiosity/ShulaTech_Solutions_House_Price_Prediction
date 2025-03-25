fb164993c62c3bf233af85012076d30f1f4499fe
code-curiosity <hitanshbhagtani@gmail.com>
2025-03-25 12:46:52 +0530

    Added training snd testing datasets

--- housePricePrediction.py
+++ housePricePrediction.py
@@ -1,3 +1,5 @@
+# House Price Prediction using Machine Learning
+# Importing necessary libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,3 +7,16 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the training dataset
+data = pd.read_csv('train.csv')
+
+# Keeping only essential features
+essential_features = ['LotArea', 'Neighborhood', 'GrLivArea', 'BedroomAbvGr', 'SalePrice']
+data = data[essential_features]
+
+# One-hot encoding for 'Neighborhood'
+data = pd.get_dummies(data, columns=['Neighborhood'], drop_first=True)
+
+# Loading test data
+test_data = pd.read_csv("test.csv")
